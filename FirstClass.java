08c90741ed724975324e153c07e3ae5540b5a42b
Max Leuku Rudander <max.leuku@gmail.com>
2018-03-07 00:54:51 +0100

    Re-re

---
+++ FirstClass.java
@@ -0,0 +1,12 @@
+package annasPaket;
+
+public class FirstClass {
+	public void firstMethod() {
+		System.out.println("Ostbågar är gula och gula i mitt hjärta.");
+	}
+	public static void main (String [] args) {
+		FirstClass fc = new FirstClass();
+		fc.firstMethod();
+	}
+
+}
